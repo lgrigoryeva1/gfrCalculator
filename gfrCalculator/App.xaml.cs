ee2ce4262291c3df120757241df1a4b478f57bae
lgrigoryeva1 <lgrigoryeva1996@gmail.com>
2018-06-06 12:04:01 +0300

    Initial commit

---
+++ gfrCalculator/App.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Globalization;
+
+namespace gfrCalculator
+{
+    
+    public partial class App : Application
+    {
+        private static List<CultureInfo> m_Languages = new List<CultureInfo>();
+
+        public static List<CultureInfo> Languages
+        {
+            get
+            {
+                return m_Languages;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+            App.LanguageChanged += App_LanguageChanged;
+            m_Languages.Clear();
+            m_Languages.Add(new CultureInfo("en-US")); // default language
+            m_Languages.Add(new CultureInfo("ru-RU"));
+
+            Language = gfrCalculator.Properties.Settings.Default.DefaultLanguage;
+        }
+
+        public static event EventHandler LanguageChanged;
+
+        public static CultureInfo Language
+        {
+            get
+            {
+                return System.Threading.Thread.CurrentThread.CurrentUICulture;
+            }
+            set
+            {
+                if (value == null) throw new ArgumentNullException("value");
+                if (value == System.Threading.Thread.CurrentThread.CurrentUICulture) return;
+
+                //Changing the language
+                System.Threading.Thread.CurrentThread.CurrentUICulture = value;
+
+                //Creating ResourceDictionary for the new language, Russian in our case
+                ResourceDictionary dict = new ResourceDictionary();
+                switch (value.Name)
+                {
+                    case "ru-RU":
+                        dict.Source = new Uri(String.Format("Resources/lang.{0}.xaml", value.Name), UriKind.Relative);
+                        break;
+                    default:
+                        dict.Source = new Uri("Resources/lang.xaml", UriKind.Relative);
+                        break;
+                }
+
+                //Replacing old ResourceDictionary with the one we just created
+                ResourceDictionary oldDict = (from d in Application.Current.Resources.MergedDictionaries
+                                              where d.Source != null && d.Source.OriginalString.StartsWith("Resources/lang.")
+                                              select d).First();
+                if (oldDict != null)
+                {
+                    int ind = Application.Current.Resources.MergedDictionaries.IndexOf(oldDict);
+                    Application.Current.Resources.MergedDictionaries.Remove(oldDict);
+                    Application.Current.Resources.MergedDictionaries.Insert(ind, dict);
+                }
+                else
+                {
+                    Application.Current.Resources.MergedDictionaries.Add(dict);
+                }
+
+                //Call the event for all windows
+                LanguageChanged(Application.Current, new EventArgs());
+            }
+        }
+        private void App_LanguageChanged(Object sender, EventArgs e)
+        {
+            gfrCalculator.Properties.Settings.Default.DefaultLanguage = Language;
+            gfrCalculator.Properties.Settings.Default.Save();
+        }
+
+    }
+}
---
+++ gfrCalculator/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace gfrCalculator
+{
+    public class KidneyData
+    {
+        public int Height { get; set; }
+        public double SerumValue { get; set; }
+    }
+
+    
+    public partial class MainWindow : Window
+    {       
+        int height;
+        double serum;
+        double rate;
+        
+        KidneyData Patient;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Patient = new KidneyData();
+            this.DataContext = Patient;
+
+            App.LanguageChanged += LanguageChanged;
+
+            CultureInfo currLang = App.Language;
+
+            menuLanguage.Items.Clear();
+            foreach (var lang in App.Languages)
+            {
+                MenuItem menuLang = new MenuItem();
+                menuLang.Header = lang.DisplayName;
+                menuLang.Tag = lang;
+                menuLang.IsChecked = lang.Equals(currLang);
+                menuLang.Click += ChangeLanguageClick;
+                menuLanguage.Items.Add(menuLang);
+            }
+
+            heightValue.MaxLength = 3;
+            serumValue.MaxLength = 3;
+        }
+
+        private void LanguageChanged(Object sender, EventArgs e)
+        {
+            CultureInfo currLang = App.Language;
+
+            foreach (MenuItem i in menuLanguage.Items)
+            {
+                CultureInfo ci = i.Tag as CultureInfo;
+                i.IsChecked = ci != null && ci.Equals(currLang);
+            }
+        }
+
+        private void ChangeLanguageClick(Object sender, EventArgs e)
+        {
+            MenuItem mi = sender as MenuItem;
+            if (mi != null)
+            {
+                CultureInfo lang = mi.Tag as CultureInfo;
+                if (lang != null)
+                {
+                    App.Language = lang;
+                }
+            }
+        }
+
+        private void resetButton_Click(object sender, RoutedEventArgs e)
+        {
+            heightValue.Clear();
+            serumValue.Clear();
+            resultLabel.Content = "";
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            height = Int32.Parse(heightValue.Text);
+            serum = Double.Parse(serumValue.Text);
+
+            if (mgButton.IsChecked == true)
+            {
+                rate = 0.413 * height / serum;
+                resultLabel.Content = String.Format("{0:0.00}", rate);
+            }
+
+            else if (molButton.IsChecked == true)
+            {
+                rate = 0.36 * height / serum;
+                resultLabel.Content = String.Format("{0:0.00}", rate);
+            }
+        }
+    }
+
+    public class StringToIntValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            int i;
+            if (int.TryParse(value.ToString(), out i))
+                return new ValidationResult(true, null);
+
+            return new ValidationResult(false, "Please enter a valid integer value");
+        }
+    }
+
+    public class StringToDoubleValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            double j;
+            if (double.TryParse(value.ToString(), out j))
+                return new ValidationResult(true, null);
+
+            return new ValidationResult(false, "Please enter a valid numerical value");
+        }
+    }
+}
